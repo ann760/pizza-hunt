367b3b313f313cddb77cc4a0d951d21aeda1ee80
Ann-Marie <anniedillier@gmail.com>
2020-12-01 20:16:49 -0800

    valadator

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -21,20 +21,14 @@ const pizzaController = {
   getPizzaById({ params }, res) {
     Pizza.findOne({ _id: params.id })
       .populate({
-        path: "comments",
-        select: "-__v",
+        path: 'comments',
+        select: '-__v'
       })
-      .select("-__v")
-      .then((dbPizzaData) => {
-        if (!dbPizzaData) {
-          res.status(404).json({ message: "No pizza found with this id!" });
-          return;
-        }
-        res.json(dbPizzaData);
-      })
-      .catch((err) => {
+      .select('-__v')
+      .then(dbPizzaData => res.json(dbPizzaData))
+      .catch(err => {
         console.log(err);
-        res.status(400).json(err);
+        res.sendStatus(400);
       });
   },
 
@@ -47,7 +41,7 @@ const pizzaController = {
 
   // update pizza by id
   updatePizza({ params, body }, res) {
-    Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true })
+    Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
       .then((dbPizzaData) => {
         if (!dbPizzaData) {
           res.status(404).json({ message: "No pizza found with this id!" });
@@ -61,15 +55,9 @@ const pizzaController = {
   // delete pizza
   deletePizza({ params }, res) {
     Pizza.findOneAndDelete({ _id: params.id })
-      .then((dbPizzaData) => {
-        if (!dbPizzaData) {
-          res.status(404).json({ message: "No pizza found with this id!" });
-          return;
-        }
-        res.json(dbPizzaData);
-      })
-      .catch((err) => res.status(400).json(err));
-  },
+      .then(dbPizzaData => res.json(dbPizzaData))
+      .catch(err => res.json(err));
+  }
 
 };
 
--- public/assets/js/add-pizza.js
+++ public/assets/js/add-pizza.js
@@ -67,6 +67,8 @@ const handlePizzaSubmit = event => {
     })
     .catch(err => {
       console.log(err);
+      saveRecord(formData);
+      // DO INDEXED DB STUFF HERE
     });
 };
 
